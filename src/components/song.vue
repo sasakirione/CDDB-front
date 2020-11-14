2b33b8a9f876fdd48ec0f07947af8f23bb508dfb
sasakirione <sasakirione@gmail.com>
2020-11-14 19:17:40 +0900

    SongページのJunbotronの設置

--- src/components/song.vue
+++ src/components/song.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>Song</h1>
+  <div class="jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-3">楽曲の検索</h1>
+      <p class="lead">Search for song</p>
+    </div>
+  </div>
 </template>
 
 <script>
