05d496820f385455bc944a99af8a92061404fc3f
sasakirione <sasakirione@gmail.com>
2020-11-15 00:44:59 +0900

    ページ名の変更

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,20 @@
 </template>
 
 <script>
-import myheader from "@/components/myheader";
+import myheader from "@/components/myheader"
+import info from "../package.json"
 
 export default {
   name: 'App',
   components: {
     myheader
+  },
+  data() {
+    return {
+      host: location.hostname,
+      appName: info.name.substr(0,1).toUpperCase() + info.name.substr(1),
+      version: info.version
+    }
   }
 }
 
--- src/components/song.vue
+++ src/components/song.vue
@@ -1,9 +1,23 @@
 <template>
+  <div>
   <div class="jumbotron-fluid">
     <div class="container">
       <h1 class="display-3">楽曲の検索</h1>
       <p class="lead">Search for song</p>
     </div>
+  </div>
+  <div>
+    <div class="container mx-auto">
+      <form class="form-inline mx-auto">
+        <select class="form-control" v-model="type">
+          <option>曲名から</option>
+          <option>アーティスト名から(未実装)</option>
+        </select>
+        <input class="form-control" type="search" placeholder="Search" aria-label="Search" v-model="keyword">
+        <button class="btn btn-outline-success" type="submit" v-on:click="Search()">Search</button>
+      </form>
+    </div>
+  </div>
   </div>
 </template>
 
