67e632cba2d1a7dbd504812fb93695b83f3e4124
sasakirione <sasakirione@gmail.com>
2020-11-15 09:56:37 +0900

    検索部分のデザインの仮完成

--- src/components/song.vue
+++ src/components/song.vue
@@ -6,16 +6,21 @@
       <p class="lead">Search for song</p>
     </div>
   </div>
+    <br>
+    <br>
   <div>
-    <div class="container mx-auto">
-      <form class="form-inline mx-auto">
+    <div class="container">
+      <form class="form-inline mx-auto" style="width: 500px;">
+        <div class="form-group">
         <select class="form-control" v-model="type">
           <option>曲名から</option>
           <option>アーティスト名から(未実装)</option>
         </select>
         <input class="form-control" type="search" placeholder="Search" aria-label="Search" v-model="keyword">
         <button class="btn btn-outline-success" type="submit" v-on:click="Search()">Search</button>
+        </div>
       </form>
+      <div><br>検索は完全一致です</div>
     </div>
   </div>
   </div>
